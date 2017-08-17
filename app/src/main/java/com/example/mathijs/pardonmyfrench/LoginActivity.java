579f95688c0df787e81d10a42f82f3a471dd8bc7
Mathijs Van den Broeck <mathijsvandenbroeck@gmail.com>
2017-08-17 16:35:46 +0200

    added login and register

--- app/src/main/java/com/example/mathijs/pardonmyfrench/LoginActivity.java
+++ app/src/main/java/com/example/mathijs/pardonmyfrench/LoginActivity.java
@@ -1,13 +1,124 @@
 package com.example.mathijs.pardonmyfrench;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private static final String TAG = "LoginActivity";
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    private EditText editEmail;
+    private EditText editPassword;
+    private Button btnLogin;
+    private Button btnRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        // TODO: remove
+        mAuth.signOut();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                    // go to main activity
+                    startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+                // ...
+            }
+        };
+
+        editEmail = (EditText) findViewById(R.id.editEmail);
+        editPassword = (EditText) findViewById(R.id.editPassword);
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+        btnRegister = (Button) findViewById(R.id.btnRegister);
+
+        btnLogin.setOnClickListener(this);
+        btnRegister.setOnClickListener(this);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+    public void signIn(String email, String password) {
+        // TODO: validate email and password
+        if (!validateForm(email, password)) {
+            return;
+        }
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        // TODO rewrite
+
+                        Log.d(TAG, "signInWithEmail:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Exception e = task.getException();
+                            Log.w(TAG, "signInWithEmail:failed", e);
+                            Toast.makeText(LoginActivity.this, e.getMessage().toString(), Toast.LENGTH_SHORT).show();
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+    private boolean validateForm(String email, String password) {
+        return true;
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == btnLogin) {
+            String email = editEmail.getText().toString();
+            String password = editPassword.getText().toString();
+            signIn(email, password);
+        }
+
+        if (view == btnRegister) {
+            startActivity(new Intent(this, RegisterActivity.class));
+        }
     }
 }
--- app/src/main/java/com/example/mathijs/pardonmyfrench/RegisterActivity.java
+++ app/src/main/java/com/example/mathijs/pardonmyfrench/RegisterActivity.java
@@ -1,13 +1,93 @@
 package com.example.mathijs.pardonmyfrench;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class RegisterActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final String TAG = "RegisterActivity";
+    private FirebaseAuth mAuth;
+
+    private EditText editEmail;
+    private EditText editPassword;
+    private Button btnLogin;
+    private Button btnRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        if (mAuth.getCurrentUser() != null) {
+
+            // TODO: do something else!
+            // go directly to overview
+            mAuth.signOut();
+
+            Toast.makeText(this, "logged out", Toast.LENGTH_SHORT).show();
+        }
+
+        editEmail = (EditText) findViewById(R.id.editEmail);
+        editPassword = (EditText) findViewById(R.id.editPassword);
+        btnRegister = (Button) findViewById(R.id.btnRegister);
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+
+        btnRegister.setOnClickListener(this);
+        btnLogin.setOnClickListener(this);
+    }
+
+    public void createAccount(String email, String password) {
+        // validate email and password
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        // TODO rewrite
+
+                        Log.d(TAG, "createUserWithEmail:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Exception e = task.getException();
+                            Log.w(TAG, "signInWithEmail:failed", e);
+                            Toast.makeText(RegisterActivity.this, e.getMessage().toString(),
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        if (task.isSuccessful()) {
+                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        }
+                    }
+                });
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == btnRegister) {
+            String email = editEmail.getText().toString();
+            String password = editPassword.getText().toString();
+            createAccount(email, password);
+        }
+
+        if (view == btnLogin) {
+            startActivity(new Intent(this, LoginActivity.class));
+        }
     }
 }
