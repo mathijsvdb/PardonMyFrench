caf330027c0ab14d175058612367da0096a172bf
Mathijs Van den Broeck <mathijsvandenbroeck@gmail.com>
2017-08-17 18:48:30 +0200

    added word object

---
+++ app/src/main/java/com/example/mathijs/pardonmyfrench/Objects/Word.java
@@ -0,0 +1,15 @@
+package com.example.mathijs.pardonmyfrench.Objects;
+
+public class Word {
+    public String french;
+    public String dutch;
+    public String by;
+    public int votes;
+
+    public Word(String french, String dutch, String by, int votes) {
+        this.french = french;
+        this.dutch = dutch;
+        this.by = by;
+        this.votes = votes;
+    }
+}
