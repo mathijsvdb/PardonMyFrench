7f0fdde7edb21fe318c0952d9c43527249c8c571
Mathijs Van den Broeck <mathijsvandenbroeck@gmail.com>
2017-08-18 00:23:32 +0200

    RecyclerView added

--- app/src/main/java/com/example/mathijs/pardonmyfrench/MainActivity.java
+++ app/src/main/java/com/example/mathijs/pardonmyfrench/MainActivity.java
@@ -1,13 +1,101 @@
 package com.example.mathijs.pardonmyfrench;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import com.example.mathijs.pardonmyfrench.Objects.Word;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, WordAdapter.ListItemClickListener{
+
+    private Button btnAddWord;
+    private FirebaseDatabase database;
+    private DatabaseReference tblWords;
+    private ArrayList<Word> wordList = new ArrayList<>();;
+
+    // recyclerview variables
+    private RecyclerView mRecyclerView;
+    private WordAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnAddWord = (Button) findViewById(R.id.btnAddWord);
+        btnAddWord.setOnClickListener(this);
+
+        // database shizzle
+        database = FirebaseDatabase.getInstance();
+        tblWords = database.getReference("words");
+
+        tblWords.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                Log.i("WOLLAG", "onChildAdded: " + dataSnapshot.child("french").getValue());
+
+                // add items to the array list wordList
+                Word word = new Word();
+                word.setFrench(dataSnapshot.child("french").getValue().toString());
+                word.setDutch(dataSnapshot.child("dutch").getValue().toString());
+                word.setBy("blakkie");
+                wordList.add(word);
+                mAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        // Recyclerview
+        mRecyclerView = (RecyclerView) findViewById(R.id.rv_words);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(layoutManager);
+
+        mAdapter = new WordAdapter(wordList, this);
+        mRecyclerView.setAdapter(mAdapter);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == btnAddWord) {
+            startActivity(new Intent(getApplicationContext(), AddWordActivity.class));
+        }
+    }
+
+    @Override
+    public void onListItemClick(Word word) {
+        // Go to word detail page
+        Intent intent = new Intent(getApplicationContext(), DetailActivity.class);
+
+
     }
 }
---
+++ app/src/main/java/com/example/mathijs/pardonmyfrench/WordAdapter.java
@@ -0,0 +1,76 @@
+package com.example.mathijs.pardonmyfrench;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import com.example.mathijs.pardonmyfrench.Objects.Word;
+import java.util.ArrayList;
+
+public class WordAdapter extends RecyclerView.Adapter<WordAdapter.WordViewHolder> {
+    final private ListItemClickListener mOnClickListener;
+
+    // de data die gaat gebruikt worden voor de recyclerview
+    private ArrayList<Word> mWords;
+
+    public interface ListItemClickListener {
+        void onListItemClick(Word word);
+    }
+
+    public WordAdapter(ArrayList<Word> mWords, ListItemClickListener listener) {
+        this.mWords = mWords;
+        mOnClickListener = listener;
+    }
+
+    @Override
+    public WordViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        int layoutIdForListItem = R.layout.word_list_item;
+        LayoutInflater inflater = LayoutInflater.from(context);
+        boolean shouldAttachToParentImmediately = false;
+
+        View view = inflater.inflate(layoutIdForListItem, parent, shouldAttachToParentImmediately);
+        WordViewHolder viewHolder = new WordViewHolder(view);
+
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(WordViewHolder holder, int position) {
+        Word word = mWords.get(position);
+        holder.bind(word);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mWords.size();
+    }
+
+    public class WordViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        private TextView listItemWordView;
+        private TextView listItemTranslation;
+        private Word mWord;
+
+        public WordViewHolder(View itemView) {
+            super(itemView);
+
+            listItemWordView = (TextView) itemView.findViewById(R.id.tv_item_word);
+            listItemTranslation = (TextView) itemView.findViewById(R.id.tv_item_translation);
+            itemView.setOnClickListener(this);
+        }
+
+        public void bind(Word word) {
+            mWord = word;
+            listItemWordView.setText(mWord.getFrench());
+            listItemTranslation.setText(mWord.getDutch());
+        }
+
+        @Override
+        public void onClick(View view) {
+//            int clickedPosition = getAdapterPosition();
+            mOnClickListener.onListItemClick(mWord);
+        }
+    }
+}
