ce057295bb6ef42f00314b3b3bf32e647cc7e6b6
Mathijs Van den Broeck <mathijsvandenbroeck@gmail.com>
2017-08-18 11:24:32 +0200

    sharing

--- app/src/main/java/com/example/mathijs/pardonmyfrench/DetailActivity.java
+++ app/src/main/java/com/example/mathijs/pardonmyfrench/DetailActivity.java
@@ -1,6 +1,7 @@
 package com.example.mathijs.pardonmyfrench;
 
 import android.content.Intent;
+import android.support.v4.app.ShareCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -9,11 +10,15 @@ import android.view.MenuItem;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.mathijs.pardonmyfrench.Objects.Word;
+
 public class DetailActivity extends AppCompatActivity {
     private TextView mFrench;
     private TextView mDutch;
     private TextView mBy;
     private TextView mVotes;
+
+    private Word mWord;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,22 +31,32 @@ public class DetailActivity extends AppCompatActivity {
         mBy = (TextView) findViewById(R.id.tv_by);
         mVotes = (TextView) findViewById(R.id.tv_votes);
 
+        mWord = new Word();
+
         Intent intent = getIntent();
 
         if (intent.hasExtra("word_french")) {
-            mFrench.setText(intent.getStringExtra("word_french"));
+            String french = intent.getStringExtra("word_french");
+            mFrench.setText(french);
+            mWord.setFrench(french);
         }
 
         if (intent.hasExtra("word_dutch")) {
-            mDutch.setText(intent.getStringExtra("word_dutch"));
+            String dutch = intent.getStringExtra("word_dutch");
+            mDutch.setText(dutch);
+            mWord.setDutch(dutch);
         }
 
         if (intent.hasExtra("word_by")) {
-            mBy.setText(intent.getStringExtra("word_by"));
+            String by = intent.getStringExtra("word_by");
+            mBy.setText(by);
+            mWord.setBy(by);
         }
 
         if (intent.hasExtra("word_votes")) {
-            mVotes.setText(intent.getStringExtra("word_votes"));
+            String votes = intent.getStringExtra("word_votes");
+            mVotes.setText(votes);
+            mWord.setVotes(Integer.parseInt(intent.getStringExtra("word_votes")));
         }
 
         // TODO: voting + editbtn & activity;
@@ -68,6 +83,22 @@ public class DetailActivity extends AppCompatActivity {
 //            startActivity(new Intent(this, EditWordActivity.class));
         }
 
+        if (id == R.id.menu_share) {
+            onClickShareWordButton();
+        }
+
         return super.onOptionsItemSelected(item);
+    }
+
+    private void onClickShareWordButton() {
+        String mimeType = "text/plain";
+        String title = "Share this awesome french word!";
+        String textToShare = "Weet jij wat '"  + mWord.getFrench() + "' betekent? Het is frans voor '" + mWord.getDutch() + "'!";
+
+        ShareCompat.IntentBuilder.from(this)
+                .setChooserTitle(title)
+                .setType(mimeType)
+                .setText(textToShare)
+                .startChooser();
     }
 }
